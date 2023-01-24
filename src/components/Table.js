fc226ed3d8184af1f3a322f86f86c071aaba7ed4
caio-faustino <caioexfaustino.pro@gmail.com>
2023-01-24 08:31:10 -0300

    req 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
-  tableHeaders = ['Descrição', 'Valor', 'Moeda', 'Tag', 'Método de pagamento',
-    'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
+  tableHeaders = [
+    { name: 'Tag', key: 'tag' },
+    { name: 'Descrição', key: 'description' },
+    { name: 'Valor', key: 'value' },
+    { name: 'Moeda', key: 'currency' },
+    { name: 'Método de pagamento', key: 'method' },
+    { name: 'Moeda de conversão', key: 'conversionCoin' },
+    { name: 'Câmbio utilizado', key: 'ask' },
+    { name: 'Valor convertido', key: 'convertedValue' },
+    { name: 'Editar/Excluir', key: 'buttons' },
+  ];
+
+  newObjExpenses = (expenses) => {
+    const expsNew = expenses.map((expense) => ({
+      ...expense,
+      currency: expense.exchangeRates[expense.currency].name,
+      conversionCoin: 'Real',
+      value: Number(expense.value).toFixed(2),
+      ask: Number(expense.exchangeRates[expense.currency].ask).toFixed(2),
+      convertedValue: this.valueConvertExc(expense.exchangeRates, expense),
+    }));
+    return expsNew;
+  };
+
+  valueConvertExc = (exchange, expense) => {
+    const valueTranform = Number(expense.value);
+    const excTransform = Number(exchange[expense.currency].ask);
+    const valueConvert = (excTransform * valueTranform).toFixed(2);
+    return valueConvert;
+  };
 
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        {this.tableHeaders.map((header) => <th key={ header }>{header}</th>)}
+        <thead>
+          <tr>
+            {this.tableHeaders.map((header) => (
+              <th key={ header.name }>{header.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.length !== 0 && this.newObjExpenses(expenses).map((expense) => (
+            <tr key={ expense.id }>
+              {this.tableHeaders.map((header, hIndex) => (header.key !== 'buttons' ? (
+                <td key={ hIndex }>{expense[header.key]}</td>
+              ) : (
+                <td key={ hIndex }>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              )))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
